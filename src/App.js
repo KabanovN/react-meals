f227f8448623170bffce6a667aa02e307c1550a7
Nikita <kabanov.n.v@mail.ru>
2022-04-07 16:42:53 +0300

    добавлено gh-pages, readme

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import './App.css';
 function App() {
     return (
         <>
-            <Router>
+            <Router basename='/react-meals'>
                 <Header />
                 <main className='container content'>
                     <Routes>
